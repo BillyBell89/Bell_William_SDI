a0bedae66d9819a1eefa8a5e196c39968bcc0077
BillyBell89 <charlestonlfo@gmail.com>
2015-10-01 18:30:48 -0400

    Output Assignment

--- Bell_William_Output_Assignment/js/script.js
+++ Bell_William_Output_Assignment/js/script.js
@@ -11,8 +11,18 @@ var favBand = "The Band";
 var leadSinger = "Levon Helm";
 var bandStatus = "False";
 var bandFormed = "1968"
+var birthYear = "1989";
 
-alert("My name is William Bell and I want to tell you about my favorite band " + (favBand);
+
+
+    alert("My name is William Bell and I want to tell you about my favorite band " + favBand + ".");
+    alert("My favorite song by " + favBand + "is " + favSong + ".");
+    alert("The band formed in " + bandFormed + ", " + (birthYear-bandFormed)+ "years before I was born.");
+
+
+
+
+
 
 
 
