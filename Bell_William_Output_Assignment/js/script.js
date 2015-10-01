8040aef08660e51f5f6573b608925937c223fd1c
BillyBell89 <charlestonlfo@gmail.com>
2015-10-01 18:04:55 -0400

    Output Assignment

--- Bell_William_Output_Assignment/js/script.js
+++ Bell_William_Output_Assignment/js/script.js
@@ -1,4 +1,16 @@
 /**
  * Created by BB on 10/1/15.
  */
+//William Bell
+    //SDI Section 1
+
 //create variables here (don't forget to use Camel Case)
+
+var favSong = "Ophelia";
+var favBand = "The Band";
+var leadSinger = "Levon Helm";
+var bandStatus = "False";
+var bandFormed = "1968"
+
+
+
