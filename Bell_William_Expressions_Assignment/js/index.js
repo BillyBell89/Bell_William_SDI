0a95956f60090c70d4d62ddca97892dea1bdec2b
BillyBell89 <charlestonlfo@gmail.com>
2015-10-08 18:18:36 -0400

    Expressions Assignment

--- Bell_William_Expressions_Assignment/js/index.js
+++ Bell_William_Expressions_Assignment/js/index.js
@@ -5,3 +5,17 @@
 //SDI Section 1
 
 //Planning to calculate some value based on a planet's size
+
+// test an unrelated program
+    alert ("We are going to calculate the area of a rectangle.")
+var L = prompt("Please enter the length");
+var W = prompt("Please enter the width");
+var H = prompt("Please enter the height");
+
+var area = (L * W * H)
+
+console.log("The area is" ,area);
+
+
+
+
