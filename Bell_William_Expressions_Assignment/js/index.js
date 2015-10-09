6ef77312b7ef71378ee4f012f5a42d0c379e4909
BillyBell89 <charlestonlfo@gmail.com>
2015-10-08 20:39:59 -0400

    Expressions Assignment

--- Bell_William_Expressions_Assignment/js/index.js
+++ Bell_William_Expressions_Assignment/js/index.js
@@ -7,20 +7,24 @@
 //Planning to calculate some value based on a planet's size
 
 // test an unrelated program
-    alert ("We are going to calculate the area of a rectangle.");
-var L = prompt("Please enter the length");
-var W = prompt("Please enter the width");
-var H = prompt("Please enter the height");
+    //    alert ("We are going to calculate the area of a rectangle.");
+    //var L = prompt("Please enter the length.");
+    //var W = prompt("Please enter the width.");
+    //var H = prompt("Please enter the height.");
 
-var area = (L * W * H);
+    //var area = (L * W * H);
 
 // figure out how to display string and variable in same alert
-//not working WHY?
-alert ("The area is " + area + ".");
+    //not working WHY?
+    //alert ("The area is " + area + ".");
 
 //log the answer to the console
-//working
-console.log("The area is " + area + ".");
+    //working
+    //console.log("The area is " + area + ".");
+
+//Actual Program to Run: Gravitational Force On Another Planet
+
+
 
 
 
