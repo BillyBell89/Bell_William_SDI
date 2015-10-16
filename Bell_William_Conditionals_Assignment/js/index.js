e261be1773a825c0e28a75b3f610fc5ff05c9469
BillyBell89 <charlestonlfo@gmail.com>
2015-10-15 20:41:54 -0400

    Expressions Assignment

--- Bell_William_Conditionals_Assignment/js/index.js
+++ Bell_William_Conditionals_Assignment/js/index.js
@@ -3,8 +3,20 @@
 
 //Body mass index
 
-var Weight = prompt ("What is your weight? (in Kilograms)");
-var Height = prompt("What is your height? (in Meters)");
-if(Weight=0){console.log("Weight cannot be zero")};
-if(Weight=""){console.log("Please reload and enter a weight value")};
+weight = prompt ("What is your weight? (in Kilograms)");
+height = prompt("What is your height? (in Meters)");
+//don't accept empty variable
+if(weight=""){console.log("You must enter a weight!")}
+//don't accept 0
+if(weight="0"){console.log("Weight cannot be zero!")}
+else {
+    parseInt(weight)
+}
+if(height=""){console.log("You must enter a Height!")}
+if(height="0"){console.log("Height cannot be zero!")}
+else {
+    parseInt(height);
+}
+console.log(weight*height);
+
 
