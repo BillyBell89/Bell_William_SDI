6a6e8658ace85c9756c3e48a84b754adaa2596d6
BillyBell89 <charlestonlfo@gmail.com>
2015-10-15 21:14:21 -0400

    Expressions Assignment

--- Bell_William_Conditionals_Assignment/js/index.js
+++ Bell_William_Conditionals_Assignment/js/index.js
@@ -5,7 +5,7 @@
 
 weight = prompt ("What is your weight? (in Kilograms)");
 height = prompt("What is your height? (in Meters)");
-//don't accept empty variable
+//don't accept empty or 0 variable
 if(weight==""||weight=="0"){console.log("You must enter a weight!")}
 else{
 console.log("You entered a weight of "+ weight +" kilograms.")}
@@ -13,8 +13,12 @@ console.log("You entered a weight of "+ weight +" kilograms.")}
 if(height==""||height=="0"){console.log("You must enter a height!")}
 else{
     console.log("You entered a height of "+ height +" kilograms.")}
+//conditional check for possible errors
+if(height<=weight){console.log("Please recheck your inputs for accuracy.")}
+else{
+//ensure integers for variables
 parseInt(weight);
-parseInt(height);
+parseInt(height)}
 BMI = ((weight/height)/height);
 console.log("Your body mass index is " + parseInt(BMI) + ".");
 
