2d6646f0dab3578613c48d5908e80363a3ffdf35
BillyBell89 <charlestonlfo@gmail.com>
2015-10-15 20:26:45 -0400

    Expressions Assignment

--- Bell_William_Conditionals_Assignment/js/index.js
+++ Bell_William_Conditionals_Assignment/js/index.js
@@ -1,3 +1,10 @@
-/**
- * Created by BB on 10/15/15.
- */
+//**Created by William Bell on 10/15/15.*//
+//SDI Section 1
+
+//Body mass index
+
+W = Prompt("What is your weight? (in Kilograms)");
+H = Prompt("What is your height? (in Meters)");
+if(W=0){console.log("Weight cannot be zero")};
+if(W=""){console.log("Please reload and enter a weight value")};
+
