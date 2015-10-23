39a4d248b6e03b5603281b798f973231bdb62e27
BillyBell89 <charlestonlfo@gmail.com>
2015-10-22 22:46:27 -0400

    Expressions Assignment

--- Bell_William_Functions_Assignment/js/index.js
+++ Bell_William_Functions_Assignment/js/index.js
@@ -29,16 +29,19 @@
 
 //Create function to pull lotto numbers and print them to console
 function flLotto(F1,F2,F3,F4,F5,F6){
-   console.log("Florida lotto numbers are "+ F1+ ", "+ F2+", "+F3+", "+F4+", "+F5+", and the final number is "+F6)
-}
+    if ([F1!=F2||F3||F4||F5||F6]&&[F2!=F3||F4||F5||F6]&&[F3!=F4||F5||F6]&&[F4!=F5||F6]&&[F5!=F6]) {
+        console.log("Florida lotto numbers are " + F1 + ", " + F2 + ", " + F3 + ", " + F4 + ", " + F5 + ", and the final number is " + F6)
+    }
+    }
 //create arguments using math.floor,math.random to generate numbers up to 54
 flLotto((Math.floor(Math.random()*53)+1),(Math.floor(Math.random()*53)+1),(Math.floor(Math.random()*53)+1),
     (Math.floor(Math.random()*53)+1),(Math.floor(Math.random()*53)+1),(Math.floor(Math.random()*53)+1));
 
 //Create function to pull powerball lotto numbers and print them to console
-function pbLotto(P1,P2,P3,P4,P5,PB){
-    console.log("Powerball lotto numbers are "+ P1+ ", "+ P2+", "+P3+", "+P4+", "+P5+", and the powerball is "+PB);
-
+function pbLotto(P1,P2,P3,P4,P5,PB) {
+    if ([P1!=P2||P3||P4||P5]&&[P2!=P3||P4||P5]&&[P3!=P4||P5]&&[P4!=P5]) {
+        console.log("Powerball lotto numbers are " + P1 + ", " + P2 + ", " + P3 + ", " + P4 + ", " + P5 + ", and the powerball is " + PB)
+    }
 }
 //create arguments using math.floor,math.random to generate numbers up to 59, PB up to 35
 pbLotto((Math.floor(Math.random()*59)+1),(Math.floor(Math.random()*59)+1),(Math.floor(Math.random()*59)+1),
