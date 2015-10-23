60b7a357de2f0bab8b0dd01c50306ebad43f1191
BillyBell89 <charlestonlfo@gmail.com>
2015-10-22 23:15:51 -0400

    Expressions Assignment

--- Bell_William_Functions_Assignment/js/index.js
+++ Bell_William_Functions_Assignment/js/index.js
@@ -4,32 +4,11 @@
 //William Bell
 //SDI Section 1
 
-//var diceThrow;
-//for (var throwCount = 0; throwCount < 10; throwCount++){
-    //diceThrow = (Math.floor(Math.random() * 6) + 1);
-    //console.log(diceThrow);
-//}
-
-//var whichLotto = prompt ('Which lotto would you like numbers for? (Type "Florida" or "Powerball"');
-
-//if (whichLotto=="Florida"){console.log("Florida")}
-//if (whichLotto=="Powerball"){console.log("Powerball")}
-
-//while(whichLotto!="Florida" || whichLotto) {prompt('Which lotto would you like numbers for? (Type "Florida" or "Powerball"')}
-
-//function calcArea(w, h) {
-  //  var area = w * h;
-  //  console.log(area);
-  //  return area;
-//}
-//var a = calcArea(20,30);
-
-//function powerBall(P1,P2,P3,P4,P5,PB){
-//}
-
+//Loop for prompt, require one of three responses
 while (whichLotto!="Florida"&&whichLotto!="Powerball"&&whichLotto!="Both"){var whichLotto = prompt("Which lottery numbers would you like to retrieve?('Florida', 'Powerball', or 'Both'")}
 
 //Create function to pull lotto numbers and print them to console
+//only call if response if "Florida" or "Both
 
     if (whichLotto == "Florida" || whichLotto == "Both") {
         function flLotto(F1, F2, F3, F4, F5, F6) {
@@ -44,6 +23,7 @@ while (whichLotto!="Florida"&&whichLotto!="Powerball"&&whichLotto!="Both"){var w
             (Math.floor(Math.random() * 53) + 1), (Math.floor(Math.random() * 53) + 1), (Math.floor(Math.random() * 53) + 1));
     }
 //Create function to pull powerball lotto numbers and print them to console
+//only call if response is "Powerball" or "Both"
     if (whichLotto == "Powerball" || whichLotto == "Both") {
         function pbLotto(P1, P2, P3, P4, P5, PB) {
             if ([P1 != P2 || P3 || P4 || P5] && [P2 != P3 || P4 || P5] && [P3 != P4 || P5] && [P4 != P5]) {
